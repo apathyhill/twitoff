ffdd3a219cb380305e1191b7b41e22ef026c28ee
Apathy <apathyhill@gmail.com>
2019-11-14 13:31:12 -0500

    Update models.py

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -5,14 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 DB = SQLAlchemy()
 
 class User(DB.Model):
-    user_id = DB.Column(DB.Integer, primary_key=True)
+    user_id = DB.Column(DB.BigInteger, primary_key=True)
     screen_name = DB.Column(DB.String(16), nullable=False)
     display_name = DB.Column(DB.String(16), nullable=False)
     pfp_url = DB.Column(DB.String(100), nullable=False)
     color = DB.Column(DB.String(6), nullable=False)
 
 class Tweet(DB.Model):
-    tweet_id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(280), nullable=False)
-    author_id = DB.Column(DB.Integer, nullable=False)
+    tweet_id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300), nullable=False)
+    author_id = DB.Column(DB.BigInteger, nullable=False)
     embedding = DB.Column(DB.PickleType, nullable=False)
